6835fec7d5a574a9f368511e18fb466ffe2f46ad
ayarmkt <ayat.0906@gmail.com>
2022-08-30 17:08:05 +0200

    added icon conditionally

--- src/views/WeatherDisplay.vue
+++ src/views/WeatherDisplay.vue
@@ -1,8 +1,7 @@
 <template>
-  <!-- <WeatherBG> -->
   <Background
-    :bgClassName="bgClassName ? bgClassName : 'default'"
-    :textColor="displayTextColor ? displayTextColor : null"
+    :bgClassName="stylingData.bgClassName ? stylingData.bgClassName : 'default'"
+    :textColor="stylingData.textColorClass ? stylingData.textColorClass : null"
   >
     <h1>Weather App</h1>
     <div>
@@ -10,27 +9,26 @@
         <input type="text" placeholder="Enter a city" v-model="enteredCity" />
         <button>Search</button>
       </form>
-      <div>
-        <p>notification</p>
-
+      <div v-if="fetchedWeatherData">
+        <!-- <p>notification</p> -->
         <div>{{ fetchedWeatherData.location }}</div>
         <div>
           <div>
             <div>{{ fetchedWeatherData.main }}</div>
             <div>{{ fetchedWeatherData.description }}</div>
           </div>
-          <!-- <WeatherIcon /> -->
+          <div>
+            <font-awesome-icon :icon="stylingData.iconProps" />
+          </div>
         </div>
       </div>
     </div>
   </Background>
-  <!-- </WeatherBG> -->
 </template>
 
 <script>
 import { ref, provide } from 'vue';
 import Background from '../components/Background.vue';
-
 import fetchWeatherData from '../api/fetchWeatherData';
 import renderWeatherStyling from '../functions/renderWeatherStyling';
 
@@ -39,9 +37,15 @@ export default {
   setup() {
     const API = process.env.VUE_APP_API_KEY;
     const enteredCity = ref(null);
-    const fetchedWeatherData = ref({});
-    const bgClassName = ref(null);
-    const displayTextColor = ref(null);
+    const fetchedWeatherData = ref(null);
+    const stylingData = ref({
+      bgClassName: null,
+      textColorClass: null,
+      iconProps: null,
+    });
+    // const bgClassName = ref(null);
+    // const textColorClass = ref(null);
+    // const iconProps = ref(null);
 
     const submitSearchForm = async () => {
       //FETCH WEATHER DATA
@@ -57,16 +61,24 @@ export default {
       if (fetchWeatherData) {
         const { weatherClassname, weatherIcon, textColor } =
           renderWeatherStyling(fetchedWeatherData.value.main);
-        bgClassName.value = weatherClassname;
-        displayTextColor.value = textColor;
+        stylingData.value = {
+          bgClassName: weatherClassname,
+          textColorClass: textColor,
+          iconProps: weatherIcon.props.icon,
+        };
+        // bgClassName.value = weatherClassname;
+        // textColorClass.value = textColor;
+        // iconProps.value = weatherIcon.props.icon;
       }
     };
 
     return {
       enteredCity,
       fetchedWeatherData,
-      bgClassName,
-      displayTextColor,
+      stylingData,
+      // bgClassName,
+      // textColorClass,
+      // iconProps,
       submitSearchForm,
     };
   },
